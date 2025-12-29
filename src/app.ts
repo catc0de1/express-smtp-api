05c86fe6b84f2261553cd65f8bd76c9cbb00bda9
CatC0de1 <catcode0101@gmail.com>
2025-12-29 21:39:35 +0700

    feat: add helmet

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import routes from './routes/index';
 import { swagger } from './docs/swagger';
 
 const app = express();
 
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(express.json());
 app.set('trust proxy', 1);
 
